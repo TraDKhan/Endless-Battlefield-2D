d1eeaf04eee9ec6430ca2261aa913abf7fb59f74
TraDkhan <trandkhanh04@gmail.com>
2025-12-18 12:11:07 +0700

    update animation weapon

---
+++ Assets/_01Version/Scripts/Weapon/Animation/WeaponAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WeaponAnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private string fireTrigger = "Fire";
+    [SerializeField] private string laserBool = "IsLaserFiring";
+
+    public void PlayFire()
+    {
+        animator.SetTrigger(fireTrigger);
+    }
+
+    public void StartLaser()
+    {
+        animator.SetBool(laserBool, true);
+    }
+
+    public void StopLaser()
+    {
+        animator.SetBool(laserBool, false);
+    }
+}
