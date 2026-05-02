317c972bb1a4dd766a24450030e2587c796fceb3
Tran Duy Khanh <trandkhanh04@gmail.com>
2026-05-02 16:42:20 +0700

    update lựa chọn nhân vật

---
+++ Assets/_01Version/Scripts/UI/UIHeroManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._01Version.Scripts.UI
+{
+    public class UIHeroManager : MonoBehaviour
+    {
+        public static UIHeroManager Instance;
+
+        [Header("Data")]
+        [SerializeField] private List<PlayerData> _playerDataList;
+
+        [Header("UI Slots")]
+        [SerializeField] private List<UIHeroSlot> _slots;
+
+        private int _currentIndex = -1;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            InitSlots();
+        }
+
+        private void InitSlots()
+        {
+            int count = Mathf.Min(_playerDataList.Count, _slots.Count);
+
+            for (int i = 0; i < count; i++)
+            {
+                _slots[i].Init(_playerDataList[i], i);
+            }
+        }
+
+        public void OnSlotClicked(int index)
+        {
+            _currentIndex = index;
+
+            // Update UI
+            for (int i = 0; i < _slots.Count; i++)
+            {
+                _slots[i].SetSelected(i == index);
+            }
+
+            // Set GameData
+            var selectedData = _playerDataList[index];
+            GameData.Instance.SetPlayer(selectedData);
+        }
+
+        public PlayerData GetCurrentPlayer()
+        {
+            if (_currentIndex < 0) return null;
+            return _playerDataList[_currentIndex];
+        }
+    }
+}
---
+++ Assets/_01Version/Scripts/UI/UIHeroSlot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Assets._01Version.Scripts.UI
+{
+    public class UIHeroSlot : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private TextMeshProUGUI _HeroNameText;
+        [SerializeField] private TextMeshProUGUI _HeroParameterText;
+        [SerializeField] private Image _HeroImage;
+        [SerializeField] private GameObject _IsSelect;
+
+        private int _index;
+
+        public void Init(PlayerData data, int index)
+        {
+            _index = index;
+            _IsSelect.SetActive(false);
+
+            _HeroNameText.text = data.playerName;
+            _HeroParameterText.text =
+                $"HP: {data.GetBaseStat(CharacterStatType.MaxHP)}\n" +
+                $"DEF: {data.GetBaseStat(CharacterStatType.Armor)}\n" +
+                $"SPD: {data.GetBaseStat(CharacterStatType.MoveSpeed)}";
+
+            _HeroImage.sprite = data.icon;
+        }
+
+        public void SetSelected(bool isSelected)
+        {
+            _IsSelect.SetActive(isSelected);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            UIHeroManager.Instance.OnSlotClicked(_index);
+            GameData.Instance.SetPlayer(UIHeroManager.Instance.GetCurrentPlayer());
+        }
+    }
+}
